777c87a2cf6d9f7cda395f06fd3c88e21af50da9
Alessio Limina <alessio.limina@gmail.com>
2023-04-27 20:06:28 +0200

    Exercise Crud to be completed

--- src/main/java/it/develhope/crud/Crud/controllers/CarController.java
+++ src/main/java/it/develhope/crud/Crud/controllers/CarController.java
@@ -3,10 +3,9 @@ package it.develhope.crud.Crud.controllers;
 import it.develhope.crud.Crud.entities.Car;
 import it.develhope.crud.Crud.repositories.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -19,5 +18,11 @@ public class CarController {
     public Car createCar(@RequestBody Car car){
         Car newCar = carRepository.saveAndFlush(car);
         return newCar;
+    }
+
+    @GetMapping("/")
+    public List<Car> getAllCars(){
+        List<Car> allCars = carRepository.findAll();
+        return allCars;
     }
 }
